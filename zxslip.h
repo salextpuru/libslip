5c7e50b7087eba35e41450a6c40233afa6569d77
Alexey Shamin <salex@tpu.ru>
2017-11-19 12:03:29 +0700

    Добапвлены структуры ответов

--- zxslip.h
+++ zxslip.h
@@ -3,6 +3,19 @@
 
 #include <stdint.h>
 
+/**
+* @brief Максимальное количество сокетов
+*/
+enum{zxslip_max_sockets	= 4};
+
+/**
+* @brief Максимальная длина данных
+*/
+enum{zxslip_max_datalen	= 0x400};
+
+/**
+* @brief Коды команд
+*/
 enum zxslip_cmd_ids{
 	//	Общие команды
 	zxslip_cmd_gettxtinfo	= 0x01,
@@ -26,18 +39,36 @@ enum zxslip_cmd_ids{
 	zxslip_cmd_accept	= 0x22
 };
 
+/**
+* @brief Получить текущий идентификатор пакета
+* 
+* @return uint8_t
+*/
 uint8_t zxslip_getId();
+
+/**
+* @brief Установить текущий идентификатор пакета
+*/
 void zxslip_setId(uint8_t id);
 
-// Добавить в буфер заголовок, вернет оно лишь указатель, что за буфера концом
+/**
+* @brief Добавить в буфер заголовок, вернет оно лишь указатель, что за буфера концом
+* 
+* @param buf - буфер куда помещается команда
+* @param cmd - код команды
+* @param id - идентификатор
+* @return uint8_t* - указатель на конец пакета
+*/
 uint8_t* zxslip_addheader(uint8_t* buf, uint8_t cmd, uint8_t id);
 
-// Создать команду
-#define zxslip_cr_gettxtinfo(buf) zxslip_addheader(buf, zxslip_cmd_gettxtinfo, zxslip_getId())
-#define zxslip_cr_esp_poll(buf) zxslip_addheader(buf, zxslip_cmd_esp_poll, zxslip_getId())
+
+/** --------------------- Функции создания команд --------------------- */
+
+#define zxslip_cr_gettxtinfo(buf) (zxslip_addheader(buf, zxslip_cmd_gettxtinfo, zxslip_getId()))
+#define zxslip_cr_esp_poll(buf) (zxslip_addheader(buf, zxslip_cmd_esp_poll, zxslip_getId()))
 
 uint8_t* zxslip_cr_wifi_config(uint8_t* buf, uint8_t mode, const char* name, const char* pass, uint8_t auth);
-#define	zxslip_cr_wifi_status(buf) zxslip_addheader(buf, zxslip_cmd_wifi_status, zxslip_getId())
+#define	zxslip_cr_wifi_status(buf) (zxslip_addheader(buf, zxslip_cmd_wifi_status, zxslip_getId()))
 
 uint8_t* zxslip_cr_socket(uint8_t* buf, int8_t domain, int8_t type, int8_t protocol);
 uint8_t* zxslip_cr_close(uint8_t* buf, int8_t fd);
@@ -51,5 +82,101 @@ uint8_t* zxslip_cr_bind
 uint8_t* zxslip_cr_listen
 uint8_t* zxslip_cr_accept
 */
+
+/** --------------------- Функции парсинга команд --------------------- */
+
+/**
+* @brief Заголовок пакета
+*/
+typedef struct zxslip_pkt_header{
+	uint8_t cmd_code;
+	uint8_t id;
+}zxslip_pkt_header;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_gettxtinfo
+*/
+typedef struct zxslip_apkt_gettxtinfo{
+	uint16_t	size;
+	uint8_t		text[zxslip_max_datalen];
+}zxslip_apkt_gettxtinfo;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_esp_poll
+*/
+typedef struct zxslip_apkt_esp_poll{
+	uint8_t		nsock;
+	uint8_t		sock_status[zxslip_max_sockets];
+}zxslip_apkt_esp_poll;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_wifi_config
+*/
+typedef struct zxslip_apkt_wifi_config{
+	int8_t		exit_code;
+	uint8_t		status;
+}zxslip_apkt_wifi_config;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_wifi_status
+*/
+typedef struct zxslip_apkt_wifi_status{
+	int8_t		exit_code;
+	uint8_t		status;
+	uint8_t		auth;
+	uint8_t		mode;
+	uint8_t		name[0x40];
+	uint8_t		pass[0x40];
+}zxslip_apkt_wifi_status;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_socket
+*/
+typedef struct zxslip_apkt_socket{
+	int8_t		exit_code;
+}zxslip_apkt_socket;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_close
+*/
+typedef struct zxslip_apkt_close{
+	int8_t		exit_code;
+}zxslip_apkt_close;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_fcntl
+*/
+typedef struct zxslip_apkt_fcntl{
+	int8_t		exit_code;
+	uint8_t		fd_sock;
+	uint8_t		cmd_fcntl;
+	uint8_t		data[zxslip_max_datalen];
+}zxslip_apkt_fcntl;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_connect
+*/
+typedef struct zxslip_apkt_connect{
+	int8_t		exit_code;
+}zxslip_apkt_connect;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_recv
+*/
+typedef struct zxslip_apkt_recv{
+	int8_t		exit_code;
+	uint8_t		fd_sock;
+	uint8_t		flags;
+	uint16_t	size;
+	uint8_t		data[zxslip_max_datalen];
+}zxslip_apkt_recv;
+
+/**
+* @brief Тело ответа на команду zxslip_cmd_send
+*/
+typedef struct zxslip_apkt_send{
+	int8_t		exit_code;
+}zxslip_apkt_send;
+
 
 #endif /* __ZXSLIP_H__ */
